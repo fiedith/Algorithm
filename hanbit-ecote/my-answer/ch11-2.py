3f284cc04c89232316fa86dc76ef80ac61892b82
Yoo Hyunseok <brianyhs@naver.com>
2024-04-10 22:12:39 +0900

    my-answer: ch11-2
    2회

--- hanbit-ecote/my-answer/ch11-2.py
+++ hanbit-ecote/my-answer/ch11-2.py
@@ -1,19 +1,14 @@
 # p.312 곱하기 혹은 더하기
 
-def add(a, b):
-    return a + b
+s = input()
+arr = list(s)
 
-def mul(a, b):
-    return a * b
+result = int(arr.pop(0))
 
-# input numbers as list
-numbers = input()
-
-# set initial result
-result = max(add(int(numbers[0]), int(numbers[1])), mul(int(numbers[0]), int(numbers[1])))
-
-
-for i in range(2, len(numbers)):
-    result = max(add(result, int(numbers[i])), mul(result, int(numbers[i])))
+while arr:
+    popped = int(arr.pop(0))
+    sum = result + popped
+    mul = result * popped
+    result = max(sum, mul)
 
 print(result)
