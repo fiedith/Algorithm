da6d0ef8f334c1db3b06bfaf605c40ac6512a27f
Yoo Hyunseok <49493385+fiedith@users.noreply.github.com>
2024-04-22 20:03:16 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -1,18 +1,16 @@
-# return boolean
 def is_group(string):
-	letters = list(string)
-	while letters:
-		popped = letters.pop(0)
-		if popped in letters and letters[0] != popped:
-			return False
-	return True
+    arr = list(string)
+    while arr:
+        popped = arr.pop(0)
+        if popped in arr and popped != arr[0]:
+            return False
+    return True
 
 n = int(input())
 answer = 0
-
 for i in range(n):
-	string = input()
-	if(is_group(string)):
-		answer += 1
+    word = input()
+    if is_group(word):
+        answer += 1
 
 print(answer)
