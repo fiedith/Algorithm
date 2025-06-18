b202b08043a45b327bb232ef4f6b14befeb05cb2
Yoo Hyunseok <49493385+fiedith@users.noreply.github.com>
2025-06-18 21:38:45 +0900

    [level 1] Title: 모의고사, Time: 7.89 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -3,30 +3,28 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] answers) {
         
-        int[] firstPattern = {1, 2, 3, 4, 5};   // 5개 주기
-        int[] secondPattern = {2, 1, 2, 3, 2, 4, 2, 5}; // 8개 주기
-        int[] thirdPattern = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};    // 10개 주기
+        // 찍는 패턴들
+        int[][] patterns = {
+            {1, 2, 3, 4, 5},
+            {2, 1, 2, 3, 2, 4, 2, 5},
+            {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
+        };
         
         int answersLength = answers.length;
+        int patternsLength = patterns.length;
         
-        
-        int[] counter = {0, 0, 0};
+        // 각 수포자가 맞힌 문제들 counter
+        int[] counter = new int[patternsLength];
         
         for(int i = 0; i< answersLength; i++){
-            int currentAnswer = answers[i];
-            if(currentAnswer == firstPattern[i % 5]){
-                counter[0] += 1;
-            }
-            
-            if(currentAnswer == secondPattern[i % 8]){
-                counter[1] += 1;
-            }
-            
-            if(currentAnswer == thirdPattern[i % 10]){
-                counter[2] += 1;
+            for(int j = 0; j < patternsLength; j++){
+                if(patterns[j][i % (patterns[j].length)] == answers[i]){
+                    counter[j] += 1;
+                }
             }
         }
         
+        // 제일 많이 맞힌 정답수 판별
         int highest = counter[0];
         for(int i = 1; i < counter.length; i++){
             if(counter[i] > highest){
@@ -34,6 +32,7 @@ class Solution {
             }
         }
         
+        // highest만치 맞힌 수포자들 배열로 담아 변환 후 반환
         List<Integer> result = new ArrayList<>();
         for(int i = 0; i < counter.length; i++){
             if(counter[i] == highest){
