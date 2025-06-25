96de8c1b0d1231454866861961e98df7a88574f3
Yoo Hyunseok <49493385+fiedith@users.noreply.github.com>
2025-06-25 11:41:32 +0900

    [level 2] Title: 방문 길이, Time: 3.13 ms, Memory: 94.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String dirs) {
+        
+        char[] commands = dirs.toCharArray();
+
+        Map<Character, Integer[]> moves = new HashMap<>();
+        moves.put('U', new Integer[]{0, 1});
+        moves.put('D', new Integer[]{0, -1});
+        moves.put('R', new Integer[]{1, 0});
+        moves.put('L', new Integer[]{-1, 0});
+
+        Integer x = 0;
+        Integer y = 0;
+        
+        int dx = 0;
+        int dy = 0;
+
+        Set<List<Integer>> visited = new HashSet<>();
+
+        for(char command : commands){
+            Integer[] posDiff = moves.get(command);
+            dx = x + posDiff[0];
+            dy = y + posDiff[1];
+
+            // if next move is in map range
+            if(dx >= -5 && dx <= 5 && dy >= -5 && dy <= 5){
+                // 기존 좌표
+                visited.add(Arrays.asList(x+dx, y+dy));
+                x = dx;
+                y = dy;
+            }
+        }
+
+        int answer = visited.size();
+        return answer;
+    }
+}
