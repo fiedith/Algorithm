7996df6fbf4771ac2a659013f072b3e008a5db38
Yoo Hyunseok <49493385+fiedith@users.noreply.github.com>
2025-07-31 01:51:12 +0900

    [level 1] Title: 폰켓몬, Time: 6.35 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,18 +1,14 @@
 import java.util.*;
+import java.util.stream.*;
 
 class Solution {
     public int solution(int[] nums) {
-        Set<Integer> set = new HashSet<>();
-        int half = nums.length / 2;
-        for(int num : nums){
-            set.add(num);
-        }
-        int setSize = set.size();
+        Set<Integer> set = Arrays.stream(nums)
+            .boxed()
+            .collect(Collectors.toSet());
         
-        if(half > setSize){
-            return setSize;
-        } else{
-            return half;
-        }
+        int half = nums.length / 2;
+        int setSize = set.size();
+        return Math.min(half, setSize);
     }
 }
