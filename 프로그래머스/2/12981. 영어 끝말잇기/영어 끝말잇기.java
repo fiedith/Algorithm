d56db9fb7220f2fba68d289ed8330c7b99e76597
Yoo Hyunseok <49493385+fiedith@users.noreply.github.com>
2025-07-31 02:45:35 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.07 ms, Memory: 87.6 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -8,20 +8,12 @@ class Solution {
         set.add(words[0]);
         int[] answer = new int[]{0, 0};
         
-        // consecutive checker function
-        BiPredicate<String, String> isConsecutive = (past, curr) -> {
-            char[] pastArr = past.toCharArray();
-            char[] currArr = curr.toCharArray();
-            int pastLen = pastArr.length;
-            return pastArr[pastLen - 1] == currArr[0];
-        };
-        
         for(int i = 1; i < words.length; i++){
             String past = words[i - 1];
             String current = words[i];
             
             // 기존에 나온 단어가 아니고, 끝말잇기가 성공적
-            if(!set.contains(current) && isConsecutive.test(past, current)){
+            if(!set.contains(current) && past.charAt(past.length() - 1) == current.charAt(0)){
                 set.add(current);
             } else{
                 int player = (i % n) + 1;
