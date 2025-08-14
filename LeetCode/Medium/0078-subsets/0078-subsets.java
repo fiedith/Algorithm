03b096d1a8b19fd5ad0dd434196ef33338296b36
Yoo Hyunseok <49493385+fiedith@users.noreply.github.com>
2025-08-14 17:02:19 +0900

    Time: 1 ms (49.39%), Space: 42.8 MB (65.56%) - LeetHub

---
+++ LeetCode/Medium/0078-subsets/0078-subsets.java
@@ -0,0 +1,23 @@
+class Solution {
+    private static List<List<Integer>> result;
+    
+    public List<List<Integer>> subsets(int[] nums) {
+        result = new ArrayList<>();
+        List<Integer> currentList = new ArrayList<>();
+        dfs(nums, 0, currentList);
+        return result;
+    }
+
+    private static void dfs(int[] nums, int start, List<Integer> currentList){
+        if(start >= nums.length){
+            result.add(new ArrayList<>(currentList));
+            return;
+        }
+
+        // for each depth, either add one or not add one
+        currentList.add(nums[start]);
+        dfs(nums, start + 1, currentList);
+        currentList.remove(currentList.size() - 1);
+        dfs(nums, start + 1, currentList);
+    }
+}
