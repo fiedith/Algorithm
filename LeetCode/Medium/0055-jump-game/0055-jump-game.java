5ac24202e8d7f075618adcbe4384af4dd290765a
Yoo Hyunseok <49493385+fiedith@users.noreply.github.com>
2025-09-10 18:26:47 +0900

    Time: 1 ms (99.89%), Space: 45.7 MB (56.69%) - LeetHub

---
+++ LeetCode/Medium/0055-jump-game/0055-jump-game.java
@@ -0,0 +1,13 @@
+// top-down, greedy
+
+class Solution {
+    public boolean canJump(int[] nums) {
+        int target = nums.length - 1;
+        for(int i = target - 1; i >= 0; i--){
+            if(i + nums[i] >= target){
+                target = i;
+            }
+        }
+        return target == 0;
+    }
+}
