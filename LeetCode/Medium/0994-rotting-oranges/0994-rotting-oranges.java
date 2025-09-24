541ec9e8a18bca4939edf36551ab494e0059640d
Yoo Hyunseok <49493385+fiedith@users.noreply.github.com>
2025-09-24 17:50:58 +0900

    Time: 2 ms (65.57%), Space: 42.8 MB (21.11%) - LeetHub

--- LeetCode/Medium/0994-rotting-oranges/0994-rotting-oranges.java
+++ LeetCode/Medium/0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,33 +1,31 @@
 class Solution {
-    private class Node{
-        public int row;
-        public int col;
-        public int val;
-
-        public Node(int row, int col, int val){
-            this.row = row;
-            this.col = col;
-            this.val = val;
-        }
-    }
-
     public int orangesRotting(int[][] grid) {
+
         int rows = grid.length;
         int cols = grid[0].length;
 
         int INF = 987654321;
+        int fresh = 0;
 
         int[][] cost = new int[rows][cols];
         for(int i = 0; i < rows; i++){
-            Arrays.fill(cost[i], INF);
+            for(int j = 0; j < cols; j++){
+                if(grid[i][j] != 2){
+                    cost[i][j] = INF;
+                    if(grid[i][j] == 1){
+                        fresh++;
+                    }
+                }
+            }
         }
 
-        Deque<Node> q = new ArrayDeque<>();
+        // init queue, fill up with rotten
+        Deque<int[]> q = new ArrayDeque<>();
 
         for(int i = 0; i < rows; i++){
             for(int j = 0; j < cols; j++){
                 if(grid[i][j] == 2){
-                    q.addLast(new Node(i, j, 0));
+                    q.addLast(new int[]{i, j});
                 }
             }
         }
@@ -35,13 +33,14 @@ class Solution {
         int[] dx = new int[]{1, -1, 0, 0};  // rows
         int[] dy = new int[]{0, 0, 1, -1};  // cols
 
+        int result = 0;
         while(!q.isEmpty()){
-            Node now = q.pollFirst();
-            cost[now.row][now.col] = Math.min(cost[now.row][now.col], now.val);
-
+            int[] now = q.pollFirst();
+            result = Math.max(result, cost[now[0]][now[1]]);
+            
             for(int i = 0; i < 4; i++){
-                int nx = now.row + dx[i];
-                int ny = now.col + dy[i];
+                int nx = now[0] + dx[i];
+                int ny = now[1] + dy[i];
 
                 // if out of bounds or not fresh tomato(=already rotten at first place, or empty)
                 if(nx < 0 || nx >= rows || ny < 0 || ny >= cols || grid[nx][ny] != 1) {
@@ -49,27 +48,14 @@ class Solution {
                 }
 
                 // add to q only if existing min cost of the adjacent block contains larger value
-                if(cost[nx][ny] > now.val + 1){
-                    q.addLast(new Node(nx, ny, now.val + 1));
+                if(cost[nx][ny] > cost[now[0]][now[1]] + 1){
+                    q.addLast(new int[]{nx, ny});
+                    cost[nx][ny] = cost[now[0]][now[1]] + 1;
+                    fresh--;
                 }
             }
         }
 
-        int result = 0;
-
-        for(int i = 0; i < rows; i++){
-            for(int j = 0; j < cols; j++){
-                // if exists fresh orange unvisited, return -1
-                if(grid[i][j] == 1 && cost[i][j] == INF){
-                    return -1;
-                }
-
-                if(cost[i][j] != INF){
-                    result = Math.max(result, cost[i][j]);
-                }
-            }
-        }
-
-        return result;
+        return fresh == 0 ? result : -1;
     }
 }
