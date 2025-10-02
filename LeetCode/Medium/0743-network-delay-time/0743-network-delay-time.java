cd44b38c6ca8dd415ba89b46a58d6143fc5a42a8
Yoo Hyunseok <49493385+fiedith@users.noreply.github.com>
2025-10-02 15:26:34 +0900

    Time: 24 ms (15.65%), Space: 47.5 MB (90.17%) - LeetHub

--- LeetCode/Medium/0743-network-delay-time/0743-network-delay-time.java
+++ LeetCode/Medium/0743-network-delay-time/0743-network-delay-time.java
@@ -1,79 +1,48 @@
 /**
- * best case: dijkstra + min head(PQ)
+ * opt: PQ + visited[]
  */
 class Solution {
-    private class Node{
-        int dest;
-        int cost;
-
-        public Node(int dest, int cost){
-            this.dest = dest;
-            this.cost = cost;
-        }
-    }
-
     public int networkDelayTime(int[][] times, int n, int k) {
-        // init minCost arr
-        final int INF = 987654321;
-        int[] minCost = new int[n + 1];     // 1-based arr
-        Arrays.fill(minCost, INF);
-        minCost[k] = 0;     // set starting node cost
+        Set<Integer> visited = new HashSet<>(); // use set to add visited nodes -> this is required to count number of nodes visited
 
-        // K: src node number, V: list<node>
-        Map<Integer, List<Node>> map = new HashMap<>();
-        // time[src, dest, cost]
+        // K: from, V: [to, cost]
+        Map<Integer, List<int[]>> map = new HashMap<>();
+
+        // time[src node, dest node, cost]
         for(int[] time : times){
-            List<Node> list = map.getOrDefault(time[0], new ArrayList<>());
-            list.add(new Node(time[1], time[2]));
+            List<int[]> list = map.getOrDefault(time[0], new ArrayList<>());
+            list.add(new int[]{time[1], time[2]});
             map.put(time[0], list);
         }
 
-        // Deque<int[]> q = new ArrayDeque<>();
-        // use PQ instead
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
 
-        List<Node> startingAdjNodes = map.getOrDefault(k, new ArrayList<>());
-        // fast return
-        if(startingAdjNodes.isEmpty()){
-            return -1;
-        }
+        // elements: int[node no., cost]
+        Queue<int[]> pq = new PriorityQueue<>((a, b) -> Integer.compare(a[1], b[1]));     // min heap
 
-        // init q
-        // for(Node node : startingAdjNodes){
-        //     q.addLast(new int[]{node.dest, node.cost});    // int[dest no, cost]
-        // }
-
-        // just add initial [k, 0]
         pq.add(new int[]{k, 0});
 
+        int result = 0;
         while(!pq.isEmpty()){
-            int[] arr = pq.poll();
-            int now = arr[0];
-            int cost = arr[1];
-            // skip the rest if not updatable
-            if(cost > minCost[now]){
+            int[] polled = pq.poll();
+            int node = polled[0];
+            int cost = polled[1];
+            // pq always guarantees least cost -> all visited nodes are init with shortest path
+            if(visited.contains(node)){
                 continue;
             }
+            visited.add(node);
+            result = Math.max(result, cost);
 
-            for(Node next : map.getOrDefault(now, new ArrayList<>())){
-                int newCost = cost + next.cost;
-                if(newCost < minCost[next.dest]){
-                    minCost[next.dest] = newCost;
-                    pq.add(new int[]{next.dest, newCost});
+            List<int[]> adj = map.getOrDefault(node, new ArrayList<>());
+            for(int[] next : adj){
+                // only add unvisited adj nodes
+                if(!visited.contains(next[0])){
+                    pq.add(new int[]{next[0], cost + next[1]});
                 }
             }
         }
 
-        int result = 0;
-        for(int i = 1; i < n + 1; i++){
-            // if any val is INF, cannot visit all nodes
-            if(minCost[i] == INF){
-                return -1;
-            }
-            // the max cost required to reach any node will be the minimum time
-            result = Math.max(result, minCost[i]);
-        }
-
-        return result;
+        // if all nodes are visited, return result else -1
+        return visited.size() == n ? result : -1;
     }
 }
