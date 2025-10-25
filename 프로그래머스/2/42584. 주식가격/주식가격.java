4184415614ce986e704942e5be828318f3ecac71
Yoo Hyunseok <49493385+fiedith@users.noreply.github.com>
2025-10-25 14:24:49 +0900

    [level 2] Title: 주식가격, Time: 33.28 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,22 +1,27 @@
 import java.util.*;
 
-public class Solution {
-    public static int[] solution(int[] prices) {
-
-        int arrLen = prices.length;
-        int[] answer = new int[arrLen];
-
-        A: for(int i = 0; i < arrLen - 1; i++) {
-            for(int j = i; j < arrLen - 1; j++){
-                answer[i]++;
-                // 다음 가격에서 하락하는 경우 inner loop 탈출
-                if(prices[i] > prices[j + 1]) {
-                    continue A;
-                }
+class Solution {
+    public int[] solution(int[] prices) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        stack.push(0);
+        
+        int[] result = new int[prices.length];
+        
+        for(int i = 1; i < prices.length; i++){
+            // if price fall occurs, pop til the point where price fall stops
+            while(!stack.isEmpty() && prices[i] < prices[stack.peek()]){
+                int poppedIdx = stack.pop();
+                result[poppedIdx] = i - poppedIdx;
             }
+            stack.push(i);
         }
-        // 마지막 요소는 0으로 기록
-        answer[arrLen - 1] = 0;
-        return answer;
+        
+        // remaining idxs = no price fall occurred
+        while(!stack.isEmpty()){
+            int poppedIdx = stack.pop();
+            result[poppedIdx] = prices.length - 1 - poppedIdx;
+        }
+        
+        return result;
     }
 }
