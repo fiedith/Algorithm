2a4769c5add4caf084c4ed6c5deff48dc3d57496
Yoo Hyunseok <49493385+fiedith@users.noreply.github.com>
2025-10-30 15:21:21 +0900

    Time: 934 ms (7.22%), Space: 52.3 MB (25.61%) - LeetHub

---
+++ LeetCode/Medium/0015-3sum/0015-3sum.java
@@ -0,0 +1,33 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+
+        Set<List<Integer>> set = new HashSet<>();
+
+        for(int i = 0; i < nums.length - 2; i++){
+            int target = -nums[i];
+            // since array is sorted, if current target < 0, cannot form sum to target
+            if(target < 0){
+                break;
+            }
+
+            int lp = i + 1;
+            int rp = nums.length - 1;
+
+            while(lp < rp){
+                int sum = nums[lp] + nums[rp];
+
+                if(sum == target){
+                    set.add(List.of(nums[i], nums[lp], nums[rp]));
+                    rp--;   // try to search for other combinations within current possible range
+                } else if(sum < target){
+                    lp++;
+                } else{
+                    rp--;
+                }
+            }
+        }
+
+        return new ArrayList<>(set);
+    }
+}
