f666b1699536898110ee9212e3bf922efbbc1318
Yoo Hyunseok <49493385+fiedith@users.noreply.github.com>
2025-11-26 14:45:40 +0900

    [level 2] Title: 기능개발, Time: 7.77 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,34 +1,46 @@
 import java.util.*;
 
-// 각 작업별 배포일자를 먼저 계산해서 복잡도 낮추는 enhancement
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        
-        int n = progresses.length;
-        int[] releaseDays = new int[n];    // deployable days per job
-        List<Integer> result = new ArrayList<>();
-        
-        // init releases
-        for(int i = 0; i < n; i++){
-            releaseDays[i] = (int)Math.ceil((100.0 - progresses[i]) / speeds[i]);
+        int len = progresses.length;
+        // edge case
+        if(len == 1){
+            return new int[]{1};
         }
         
-        // init head release day
-        int head = releaseDays[0];
-        int tasks = 0;  // deployable jobs per release
+        int[] daysToDeploy = new int[len];
         
-        for(int i = 0; i < n; i++){
-            if(releaseDays[i] <= head){
-                tasks++;
-            } else{
-                result.add(tasks);  // add deployable job count in result
-                head = releaseDays[i];  // set next head job at current
-                tasks = 1;
+        for(int i = 0; i < len; i++){
+            // remaining task time
+            daysToDeploy[i] = 100 - progresses[i];  
+            // round up
+            if(daysToDeploy[i] % speeds[i] != 0){
+                daysToDeploy[i] = (daysToDeploy[i] / speeds[i]) + 1;
+            } else {
+                daysToDeploy[i] = daysToDeploy[i] / speeds[i];
             }
         }
-        result.add(tasks);  // add remaining
         
-        return result.stream().mapToInt(Integer::intValue).toArray();
+        // base point
+        int bp = daysToDeploy[0];
+        int counter = 1;
+        List<Integer> result = new ArrayList<>();
         
+        // stack unnecessary, can simply loop in arr
+        for(int i = 1; i < len; i++){
+            if(daysToDeploy[i] <= bp) {
+                counter++;
+            } else {
+                bp = daysToDeploy[i];
+                result.add(counter);
+                counter = 1;    // reset counter
+            }
+        }
+        
+        result.add(counter);    // add last one
+        
+        return result.stream()
+            .mapToInt(Integer::intValue)
+            .toArray();
     }
 }
